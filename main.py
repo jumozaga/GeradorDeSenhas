b04b3f75314f5399a58e67a02c16be426dcb107e
Juliana <61429635+jumozaga@users.noreply.github.com>
2021-07-27 15:09:21 +0000

    Rmv Comentario e melhor força da senha

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import string
 from random import random, choice
 
-#print (string.ascii_letters) #todas as letras + string.dig
-#print (string.digits) #numeros de 0 a 9
-#print (string.punctuation) # caracteres especiais
 valores = string.ascii_letters
 valores += string.digits
 valores += string.punctuation
-tamanho = 7
+tamanho = 11
 senha = ""
-#print (valores)
 
 for i in range(tamanho):
     senha += choice(valores)
